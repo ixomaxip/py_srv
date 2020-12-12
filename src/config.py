e75ae1bdf7df60faf7c4daf71917d09e748cc161
ixomaxip <ixomaxip@gmail.com>
2020-12-12 16:20:09 +0500

    /info

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 import os
 from tools.env_tool import get_env
+import platform
 
 
 debug = get_env("DEBUG", bool, False)
 port = get_env("PORT", int, 80)
+hostname = platform.node()
 
 
 class AttrDict(dict):
--- src/srv.py
+++ src/srv.py
@@ -10,17 +10,28 @@ import os
 import time
 import numpy as np
 import tools.utils as utils
+from datetime import datetime
 
 from aiohttp import web
 from aiohttp.web_exceptions import HTTPException
 
 log = utils.get_logger('srv', sys.stdout, debug_level=config.debug)
 
+up_time = datetime.now()
+
 routes = web.RouteTableDef()
 
 @routes.get('/ping')
 async def ping(request: web.Request):
     return web.Response(text='pong')
+
+@routes.get('/info')
+@routes.post('/info')
+async def info(request: web.Request):
+    resp = {'hostname': config.hostname, 'up_time': str(datetime.now() - up_time)}
+    log.info(json.dumps(resp))
+
+    return web.json_response(resp)
 
 
 app = web.Application(logger=log)
