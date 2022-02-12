9ee10c0a652b684d9f3e58ec6d0cafd85cda4ee5
ixomaxip <ixomaxip@gmail.com>
2022-02-12 23:46:56 +0500

    add omegaconf for config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,51 @@
 import os
-from tools.env_tool import get_env
-import platform
+import json
+import logging
+import traceback
+from omegaconf import OmegaConf, DictConfig
 
+from tools.logger import prepare_logger
 
-debug = get_env("DEBUG", bool, False)
-port = get_env("PORT", int, 80)
-hostname = platform.node()
+class SrvConfig(DictConfig):
+
+    def __init__(self):
+        super().__init__({})
+        default_logging = DictConfig({
+            'json_log_path': None,
+            'log_level': 'DEBUG',
+            'log_format': '%(asctime)s - %(levelname)s - %(message)s'
+        })
+        
+        yaml_options = self._read_yaml()
+
+        sub_options1 = DictConfig({})
+        sub_options2 = DictConfig({})
+
+        self.logging = OmegaConf.merge(default_logging, yaml_options.logging)
+        self.bind_host = yaml_options.get('bind_host', '127.0.0.1')
+        self.bind_port = yaml_options.get('bind_port', 80)
+        
+        self.sub_options1 = OmegaConf.merge(sub_options1, yaml_options.sub_options1)
+        self.sub_options2 = OmegaConf.merge(sub_options2, yaml_options.sub_options2)
+
+    def _read_yaml(self):
+        path = os.path.dirname(os.path.realpath(__file__))
+        config_filename = os.path.join(path, 'config.yaml')
+        try:
+            yaml_options = OmegaConf.load(config_filename)
+        except Exception as err:
+            traceback.print_exc()
+            yaml_options = DictConfig({'logging': {}})
+
+        return yaml_options
+
+    def __str__(self):
+        return json.dumps(OmegaConf.to_container(self), indent=4)
+
+    def to_dict(self):
+        return OmegaConf.to_container(self)
+
+config = SrvConfig()
+
+logger = logging.getLogger()
+prepare_logger(logger, config.logging)
